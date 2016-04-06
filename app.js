b640c3acccf9f4137b6ea80304cdc9e7de506f67
Gustav Genberg <gurra.genberg@gmail.com>
2016-04-06 19:36:56 +0200

    Express server added & config file added

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var config = require('./config.js');
+var io = require('socket.io')(config.socket_port);
+
+app.listen(config.express_port, function () {
+  console.log('Express server running on port ' + config.express_port);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/http/index.html');
+});
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  socket_port: 7777,
+  express_port: 8888
+};
